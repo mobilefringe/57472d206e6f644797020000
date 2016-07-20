f44048b60df5bd50f6a9da280993a41224a01bec
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-07-20 11:34:17 -0400

    The District at Tustin Legacy 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "The District at Tustin Legacy",
                     "default_image" : "http://assets.codecloudapp.com/sites/57472d206e6f644797020000/image/jpeg/undefined/dtl_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "196554225"
+                    "social_feed" : "http://thedistrict.mallmaverick.com/api/v2/district/social.json"
                 }
