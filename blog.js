0653c095bc9f0f5d2c896252e1bbcb47d48dcd2e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-23 13:48:08 -0500

    The District at Tustin Legacy 1.5  /blog.js

--- blog.js
+++ blog.js
@@ -29,6 +29,12 @@ function regularPostList () {
     var published_posts = posts.sortBy(function(o){ return moment(o.publish_date) }).reverse();
     renderPosts("#blog_container", "#blog_template", published_posts);
     load_more(1, published_posts);
+    
+    $('#load_more_posts').click(function(e){
+        var i = $('#num_loaded').val();
+        load_more(i, published_posts);
+        e.preventDefault();
+    });
 }
 
 function renderPosts(container, template, collection){
